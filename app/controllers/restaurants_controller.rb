b76c13d204eedd99a002f1a1946495701f65c9e5
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-02 17:50:40 +0900

    backend and bare frontend done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,51 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
+
+#  Controller#Action
+#     restaurant_reviews GET    /restaurants/:restaurant_id/reviews(.:format)          reviews#index
+#                        POST   /restaurants/:restaurant_id/reviews(.:format)          reviews#create
+#  new_restaurant_review GET    /restaurants/:restaurant_id/reviews/new(.:format)      reviews#new
+# edit_restaurant_review GET    /restaurants/:restaurant_id/reviews/:id/edit(.:format) reviews#edit
+#      restaurant_review GET    /restaurants/:restaurant_id/reviews/:id(.:format)      reviews#show
+#                        PATCH  /restaurants/:restaurant_id/reviews/:id(.:format)      reviews#update
+#                        PUT    /restaurants/:restaurant_id/reviews/:id(.:format)      reviews#update
+#                        DELETE /restaurants/:restaurant_id/reviews/:id(.:format)      reviews#destroy
+#            restaurants GET    /restaurants(.:format)                                 restaurants#index
+#                        POST   /restaurants(.:format)                                 restaurants#create
+#         new_restaurant GET    /restaurants/new(.:format)                             restaurants#new
+#        edit_restaurant GET    /restaurants/:id/edit(.:format)                        restaurants#edit
+#             restaurant GET    /restaurants/:id(.:format)                             restaurants#show
+#                        PATCH  /restaurants/:id(.:format)                             restaurants#update
+#                        PUT    /restaurants/:id(.:format)                             restaurants#update
+#                        DELETE /restaurants/:id(.:format)                             restaurants#destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+puts 'Cleaning up db'
+Restaurant.destroy_all
+Review.destroy_all
+puts 'Cleaning up done'
+
+5.times do
+  new_restaurant = Restaurant.create({
+                                       name: Faker::Restaurant.name,
+                                       category: CATEGORIES.sample,
+                                       address: Faker::Address.street_address
+                                     })
+  puts "Created restaurant #{new_restaurant.id}"
+  rand(1..3).times do
+    new_review = Review.new({
+                              content: Faker::Restaurant.review,
+                              rating: rand(0..5)
+                            })
+    new_review.restaurant = new_restaurant
+    new_review.save
+    puts "Created review #{new_review.id} for #{new_restaurant.id}"
+  end
+end
